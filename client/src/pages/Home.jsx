87f636e406c148b3a981a0c8a41d117ad20492e5
NotZarif <flacko.programming@gmail.com>
2025-04-30 23:23:09 +0800

    feat(footer): add footer section to the page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,15 +3,16 @@ import Navbar from '../components/layout/Navbar'
 import HeroSection from '../components/sections/HeroSection/HeroSection'
 import VisionSection from '../components/sections/VisionSection/VisionSection'
 import FaqSection from '../components/sections/FaqSection/FaqSection'
+import Footer from '../components/layout/Footer'
 
 const Home = () => {
-
   return (
     <main className='max-w-[800px] mx-auto '>
       <Navbar/>
       <HeroSection/>
       <VisionSection/>
       <FaqSection/>
+      <Footer/>
     </main>
   )
 }
