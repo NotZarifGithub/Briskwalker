e2ad324b833672b88f05f0a18eff03c6cbb308fc
NotZarif <flacko.programming@gmail.com>
2025-05-03 03:37:46 +0800

    feat: add closing mobile menu on resize function

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,43 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '../common/Button'
 import Logo from '../common/logo'
+import { BsList, BsX } from "react-icons/bs";
+
+const navbarList = [
+  "Home",
+  "Journeys",
+  "Vision",
+  "FAQ",
+]
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Close mobile menu on screen resize >= sm
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 640) {
+        setIsOpen(false)
+      }
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <header
       className='flex justify-center items-center gap-2'
     >
+      <div className='flex items-center gap-2'>
       <div
         className='bg-[#232323] flex items-center p-1 rounded-md outline outline-black gap-2' 
       >
         <Logo />
 
         {/* Buttons */}
-        <div className='flex'>
+        <div className='hidden sm:flex'>
+          {navbarList.map((item, index) => (
+            <Button
+              key={index}
+              children={item}
+              onClick={() => console.log(item)}
+            />
+          ))}
+        </div>
 
-          {/* Home button */}
-          <Button 
-            onClick={() => console.log("hover")}
-            children={"Home"}
-          />
-
-          {/* Journeys button */}
-          <Button 
-            onClick={() => console.log("hover")}
-            children={"Journeys"}
-          />
-
-          {/* Vision button */}
-          <Button 
-            onClick={() => console.log("hover")}
-            children={"Vision"}
-          />
-
-          {/* FAQ button */}
-          <Button 
-            onClick={() => console.log("hover")}
-            children={"FAQ"}
-          />
+        {/* Hamburger */}
+        <div>
+          {isOpen ? 
+            <BsX
+              size={30}
+              onClick={() => (setIsOpen(!isOpen))}
+              className='text-white sm:hidden'
+            />
+          :
+            <BsList 
+              size={30}
+              onClick={() => (setIsOpen(!isOpen))}
+              className='text-white sm:hidden'         
+            />
+          }
         </div>
       </div>
 
@@ -46,6 +69,19 @@ const Navbar = () => {
         className='px-5 py-3 outline-2 outline-black bg-[#232323] hover:rounded-'
         children={"Coming Soon"}
       />
+      </div>
+
+      {isOpen && (
+      <div className='absolute bg-[#232323] flex flex-col mt-60 items-center sm:hidden p-3 rounded-md outline outline-black gap-2'>
+        {navbarList.map((item, index) => (
+          <Button
+            key={index}
+            children={item}
+            onClick={() => console.log(item)}
+          />          
+        ))}
+      </div>
+      )}
    </header>
   )
 }
